2c023eb07dcc22aab8bd36c436c06da9575fda0b
Nuran Rangika Kasthuriarachchi <nurangika@gmail.com>
2014-05-19 21:05:13 +0530

    First commit

---
+++ src/Map/IndestructibleElement.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Map;
+
+/**
+ *
+ * @author Nuran
+ */
+public class IndestructibleElement extends MapElement {
+
+    public IndestructibleElement(int x, int y) {
+        super(x, y);
+    }
+}
